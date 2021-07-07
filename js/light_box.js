74790548b4759fbe9a3e5d212ea16c92bd2b8de6
Benjamin Miles <bmiles@nethealthshops.com>
2021-07-07 15:41:45 -0500

    added methods and properties to classes

--- js/light_box.js
+++ js/light_box.js
@@ -1,53 +1,80 @@
-var bgColorImgBox = 'rgba(0,0,0,0.9)'
-var allowHideScrollImgBox = 'yes'
-var useFadeInoutImgBox = 'yes'
-var speedImgBox = 0.08
-var zIndexDvImgBox = 999
-var vopaImgBox, idpopupImgBox
+let bgColorImgBox = 'rgba(0,0,0,0.9)'
+let allowHideScrollImgBox = 'yes'
+let useFadeInoutImgBox = 'yes'
+let speedImgBox = 0.08
+let zIndexDvImgBox = 999
+let vopaImgBox, idpopupImgBox
+let gallarySelectedImage = 0
 
-function gallaryImage() {
+
+
+
+class GallaryImage {
+  
+  constructor (imgUrl, imgAlt) {
+    this.imgUrl = imgUrl
+    this.altText = imgAlt
+  }
+
+
+  getImageUrl() {
+    return this.imageURL
+  }
+
+  getImageAlt() {
+    return this.altText
+  }
+
+}
+
+class GallaryImages {
+  
+  imageArray = new Array()
+  
+  constructor() {
+    this.imageArray = []
+  }
+  
+  addImage(gallaryImage) {
     
-    let imageURL = []
-    let altText = []
+    this.imageArray.push(gallaryImage)
+    console.log('added Image')
+  }
 
-    this.addImage = function(imgUrl, imgAlt) {
-        imageURL.push(imgUrl, imgAlt)
-    }
-
-    this.imageURL = function() {
-        return this.imageURL
-    }
-
-    this.altText = function() {
-        return this.altText
-    }
+  Images() {
+    return this.imageArray
+  }
 }
 
-function gallaryImages() {
-    let images = []
+var lBauxImages = new GallaryImages()
+var image1 = new GallaryImage('url1', 'alt1')
+var image2 = new GallaryImage('url2', 'alt2')
+var image3 = new GallaryImage('url3', 'alt3')
+var image4 = new GallaryImage('url4', 'alt4')
+var image5 = new GallaryImage('url5', 'alt5')
+var image6 = new GallaryImage('url6', 'alt6')
 
-    this.addImage = function (gallaryImage) {
-        images.push(gallaryImage)
-    }
-
-    this.returnImages = function() {
-        return images
-    }
-}
-
+lBauxImages.addImage(image1)
+lBauxImages.addImage(image2)
+lBauxImages.addImage(image3)
+lBauxImages.addImage(image4)
+lBauxImages.addImage(image5)
+lBauxImages.addImage(image6)
 
 
 window.onload = function () {
   var crtdvImgBox = document.createElement('div')
   var crtdvImgGal = document.createElement('div')
-
+  var ctrdvImages = document.createElement('div')
   crtdvImgGal.id = 'imgGal'
-  crtdvImgBox.id = 'img_box'
+  crtdvImgBox.id = 'displayedZoomedImage'
+  ctrdvImages.id = 'imgGalImages'
   idpopupImgBox = document.getElementById('imgGal')
   document.getElementsByTagName('body')[0].appendChild(crtdvImgGal)
   idImgGal = document.getElementById('imgGal')
   idImgGal.appendChild(crtdvImgBox)
-  idpopupImgBox = document.getElementById('img_box')
+  idImgGal.appendChild(ctrdvImages)
+  idpopupImgBox = document.getElementById('displayedZoomedImage')
   idpopupImgBox.style.top = 0
   idpopupImgBox.style.left = 0
   idpopupImgBox.style.opacity = 0
@@ -59,22 +86,31 @@ window.onload = function () {
   idpopupImgBox.style.textAlign = 'center'
   idpopupImgBox.style.zIndex = zIndexDvImgBox
   idpopupImgBox.style.backgroundColor = bgColorImgBox
+  idpopupImgBox.style.color = 'white'
+  idpopupImgBox.style.fontFamily = 'Arial, Helvetica, sans-serif'
+
 }
 
 
-function light_box(self, altTexgt) {
+function light_box(self, altText) {
   var namepicImgBox = typeof self === 'string' ? self : self.src
   vopaImgBox = 0
   var hwinImgBox = window.innerHeight
   var wwinImgBox = window.innerWidth
   var himgImgBox, padtopImgBox, idfadeinImgBox
   var imgImgBox = new Image()
+  var text = String()
   imgImgBox.src = namepicImgBox
-  imgImgBox.title = altTexgt
+  imgImgBox.title = altText
   imgImgBox.onload = function () {
     himgImgBox = imgImgBox.height
     var wimgImgBox = imgImgBox.width
-    idpopupImgBox.innerHTML = '<img src=' + namepicImgBox + '>' + '<h1>' + imgImgBox.title + '</h1>'
+    c = lBauxImages.Images()
+    console.log(image3)
+    for (let i = 0; i < c.length; i++) {
+      text += c[i].imgUrl + "<br>";
+    } 
+    idpopupImgBox.innerHTML = '<img src=' + namepicImgBox + '>' + '<h1>' + imgImgBox.title + '</h1>' + text
 
     if (wimgImgBox > wwinImgBox) {
       idpopupImgBox.getElementsByTagName('img')[0].style.width = '90%'
